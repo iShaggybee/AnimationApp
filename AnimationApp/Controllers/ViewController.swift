dcc47f1c9115d31128511e4bfbb2552b6597e991
Vadim Kislov <kislov.v@gmail.com>
2022-05-16 15:00:30 +0300

    feat: change storyboard with animation

--- AnimationApp/Controllers/ViewController.swift
+++ AnimationApp/Controllers/ViewController.swift
@@ -5,15 +5,18 @@
 //  Created by Kislov Vadim on 15.05.2022.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
-
+    @IBOutlet var animationView: SpringImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func runAnimation(_ sender: SpringButton) {
+        sender.animation = "wobble"
+        sender.animate()
+    }
 }
 
